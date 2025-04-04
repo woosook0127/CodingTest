71ec5cfbdb26d3e5df2d9f116b7290aae0c35b5c
wooseok Kim <201710858@sangmyung.kr>
2025-04-04 12:09:54 +0900

    [level 2] Title: 구명보트, Time: 10.13 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,10 @@
+def solution(people, limit):
+    answer = 0
+    
+    while (True):
+        num_boats = 0
+        for i in range(len(people.copy())):
+            if limit - people[i]
+            
+    
+    return answer
